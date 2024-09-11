df3196a1c46f0b59e8e0d1717d1f9a7ed458f633
Quentin Payet <quentinpayet1203@gmail.com>
2024-09-11 18:10:53 +0200

    wip: src added

---
+++ src/script.js
@@ -0,0 +1,18 @@
+// TODO : Rename properly
+lat_lng = [47.64269230515288, 6.839473664773902];
+
+// Set up the map
+var map = L.map("map").setView(lat_lng, 16);
+
+// Setup the tiles
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+// Add a pin to the map
+var marker = L.marker(lat_lng).addTo(map);
+
+// Setup a pop-up on the map to define address
+marker.bindPopup("<b>Adresse :</b><br>20 chemin pot de fleur").openPopup();
