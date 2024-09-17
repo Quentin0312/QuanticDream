6c583732114cde4fdeeaf00a29c7f37b8e41d709
Quentin Payet <quentinpayet1203@gmail.com>
2024-09-17 17:58:12 +0200

    interactive map => coordonées et infos du HQ de Paris ajouté

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// TODO : Rename properly
-lat_lng = [47.64269230515288, 6.839473664773902];
+// Paris HQ infos
+const paris_hq_lat_lng = [48.879884, 2.408803];
+const paris_hq_infos =
+  "<b>Quantic Dream - Paris, France</b><br>30 rue Raoul Wallenberg, 75019 Paris (France)<br>+33 1 44 64 00 90";
 
 // Set up the map
-var map = L.map("map").setView(lat_lng, 16);
+var map = L.map("map").setView(paris_hq_lat_lng, 16);
 
 // Setup the tiles
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -12,7 +14,7 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 // Add a pin to the map
-var marker = L.marker(lat_lng).addTo(map);
+var marker = L.marker(paris_hq_lat_lng).addTo(map);
 
 // Add a popup to the pin to show address
-marker.bindPopup("<b>Adresse :</b><br>20 chemin pot de fleur").openPopup();
+marker.bindPopup(paris_hq_infos).openPopup();
