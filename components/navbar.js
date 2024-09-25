9d162aa1298af068ce7a6e86029e75eb74db994d
Quentin Payet <quentinpayet1203@gmail.com>
2024-09-25 17:09:59 +0200

    wip home page: navbar in js file

---
+++ components/navbar.js
@@ -0,0 +1,21 @@
+function setupNavbar() {
+    return `
+        <!-- TODO : Cacher logo quand utilisateur sur la page d'accueil avec le logo déjà affiché -->
+        <div>
+            <img src="../assets/images/qd_logo_blue.svg" alt="Logo de Quantic Dream">
+        </div>
+        <div>
+            <a href="../index.html" class="active">Accueil</a>
+            <a>Histoire</a>
+            <a>Studios</a>
+            <a>Œuvres</a>
+            <a>Résultats financiers</a>
+            <a>Écologie</a>
+        </div>
+    `
+}
+
+export default function insertNavbar() {
+    const navElement = document.getElementsByTagName("navbar");
+    navElement.innerHTML = setupNavbar();
+}
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import setupMap from "./components/map.js"
 import setupCards from "./components/presentationCards.js";
+import setupNavbar from "./components/navbar.js";
+
+// Insert navbar
+setupNavbar();
 
 // Insert interactive map in the footer
 setupMap();
