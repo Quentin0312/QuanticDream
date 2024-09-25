98e762c656aedb59714c4378ea5e81f5e720db3b
Quentin Payet <quentinpayet1203@gmail.com>
2024-09-25 17:24:27 +0200

    wip home page: footer in js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-import setupMap from "./components/map.js"
-import setupCards from "./components/presentationCards.js";
 import setupNavbar from "./components/navbar.js";
+import setupCards from "./components/presentationCards.js";
+import setupFooter from "./components/footer.js";
 
 // Insert navbar
 setupNavbar();
 
-// Insert interactive map in the footer
-setupMap();
-
 // Insert presentations cards
 setupCards();
+
+// Insert footer
+setupFooter();
