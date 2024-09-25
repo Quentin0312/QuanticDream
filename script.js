16e7c03fc07e794b7ade0e9b66b008057ae9cc70
Quentin Payet <quentinpayet1203@gmail.com>
2024-09-25 18:13:17 +0200

    presentationCard.js deleted, presentations cards raw html writed

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 import setupNavbar from "./components/navbar.js";
-import setupCards from "./components/presentationCards.js";
 import setupFooter from "./components/footer.js";
 
 // Insert navbar
 setupNavbar();
-
-// Insert presentations cards
-setupCards();
 
 // TODO : Check url to specify who is the page author into setupFooter ?
 console.log(window.location.href);
