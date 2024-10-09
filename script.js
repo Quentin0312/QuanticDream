827f476fea18cb0f3619204294c541436d7a6387
Quentin Payet <quentinpayet1203@gmail.com>
2024-10-09 14:03:21 +0200

    comment added

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ import insertNavbar from "./components/navbar.js";
 import insertFooter from "./components/footer.js";
 import insertHead from "./components/head.js";
 
-// TODO : Fix pages display use css after the pafe is loaded, looking like a visual glitch
+// TODO : Fix pages display use css after the page is loaded, looking like a visual glitch
+// => Display quantic dream logo loading bar !!
+
 // https://www.flowradar.com/answer/javascript-function-delay-page-load-load-sub-page-in-webflow
 
 // Insert head
