06df95521ca479f4aa242996c038198942f1905b
Quentin Payet <quentinpayet1203@gmail.com>
2024-10-16 20:34:11 +0200

    studios.html => style enhanced (images sizes, bold words)

--- components/footer.js
+++ components/footer.js
@@ -9,7 +9,7 @@ function getPageAuhtor(activePage) {
     case "histoire":
       return "Kheira OUADAH";
     case "studios":
-      return "Quentin PAYET et Gabin MOREL";
+      return "Quentin PAYET";
     case "economie":
       return "Ethan RIETZ";
     case "ecologie":
