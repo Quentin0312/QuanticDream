32a5cbabc1181c7f6e5d026424ea8d6dcaf4f7ef
Quentin Payet <quentinpayet1203@gmail.com>
2024-10-17 14:53:24 +0200

    assets/images/oeuvres/quantic_dream_games_compressed.jpg added

--- components/utils.js
+++ components/utils.js
@@ -1,10 +1,11 @@
-export function getActivePage (){
-    // TODO : Clean ! (This is a quick fix of 'Accueil' not being 'active' because prod url end with '/QuanticDream/') !
+export function getActivePage() {
+  // TODO : Clean ! (This is a quick fix of 'Accueil' not being 'active' because prod url end with '/QuanticDream/') !
 
-    const url = window.location.href;
-    if (url === "https://quentin0312.github.io/QuanticDream/") {
-        return "index";
-    } else {
-        return url.split("/").at(-1).split(".").at(0);
-    }
+  const url = window.location.href;
+  // TODO : Ajouter gitlab et bitbucket urls
+  if (url === "https://quentin0312.github.io/QuanticDream/") {
+    return "index";
+  } else {
+    return url.split("/").at(-1).split(".").at(0);
+  }
 }
